02d177885b0251d6611366d89243400f2b7ced71
Gbolahan <oshotekungbolahan@gmail.com>
2024-06-25 14:45:41 +0100

    commit changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,24 +1,25 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 import '../styles/Hero.css';
 import divideImg from '../src/assets/pattern-divider-desktop.svg';
 import dice from '../src/assets/icon-dice.svg';
 
 const Hero = () => {
-    const [advice, setAdvice] = useState('');
-    const [adviceId, setAdviceId] = useState(null);
+    const [quote, setQuote] = useState('');
+    const [quoteId, setQuoteId] = useState(null);
   
-    const fetchAdvice = async () => {
+    const bringQuote = async () => {
       try {
         const response = await fetch('https://api.adviceslip.com/advice');
         const data = await response.json();
-        setAdvice(data.slip.advice);
-        setAdviceId(data.slip.id);
+        setQuote(data.slip.advice);
+        setQuoteId(data.slip.id);
       } catch (error) {
         console.error('Error fetching advice:', error);
       }
     };
     useEffect(() => {
-        fetchAdvice();
+        bringQuote();
       }, []);
     
   
@@ -26,14 +27,15 @@ const Hero = () => {
     <>
       <div className='card1'>
         <div className='card2 position-relative'>
-            {adviceId && <p className='text-success card-text'> ADVICE #{adviceId} </p>}
-          <p className='card-text'> {advice} </p>
+            {quoteId && <p className='text-success card-text'> ADVICE #{quoteId} </p>}
+          <p className='card-text'> {quote} </p>
           <img className='dividerImg' src={divideImg} alt="divideImg" />
         </div>
-      <button className='dice-btn  position-absolute translate-middle start-50' onClick={fetchAdvice}>
+      <button className='dice-btn  position-absolute translate-middle start-50' onClick={bringQuote}>
             <img src={dice} alt='dice' />
         </button>
       </div>
+
     </>
   );
 };
